c3a6d05e0fbc21e96e82cbcf2cfd5587f18cc0c7
Thilo Wening <thilo.wening@netways.de>
2019-03-28 08:54:38 +0100

    changed some syntax regarding the styleguide

--- icinga-smseagle-notify-contacts.py
+++ icinga-smseagle-notify-contacts.py
@@ -3,21 +3,33 @@
 import urllib
 import urllib2
 import argparse
+import sys
 
 ap = argparse.ArgumentParser()
-ap.add_argument("-u","--username", required=True, help="Username of SMSEagle HTTP API")
-ap.add_argument("-p","--password", required=True, help="Password for username SMSEagle HTTP API")
-ap.add_argument("-c","--contactname", required=True, help="Contact defined in addressbook of SMSEagle")
-ap.add_argument("-H","--hostname", required=True, help="Hostname of notified problem")
-ap.add_argument("-t","--notification-type", required=True, help="Icinga Notification type")
-ap.add_argument("-s","--state", required=True, help="Service /Host state")
-ap.add_argument("-o","--output", help="Output for Host notifications / Normal Service Output could be to long!")
-ap.add_argument("-S","--service", help="If notification is type service")
-ap.add_argument("-e","--eaglehost", required=True, help="SMSEagle HTTP API Host")
-ap.add_argument("-P","--protocol", default="http", help="set http protocol: DEFAULT=http")
-ap.add_argument("-d","--date", required=True, help="display longdatae output from icinga in sms")
-ap.add_argument("-v","--verbose", help="print verbose messages", action='store_true')
-ap.add_argument("-V","--version", help="print version", action='store_true')
+ap.add_argument("-u", "--username", required=True,
+                help="Username of SMSEagle HTTP API")
+ap.add_argument("-p", "--password", required=True,
+                help="Password for username SMSEagle HTTP API")
+ap.add_argument("-c", "--contactname", required=True,
+                help="Contact defined in addressbook of SMSEagle")
+ap.add_argument("-H", "--hostname", required=True,
+                help="Hostname of notified problem")
+ap.add_argument("-t", "--notification-type", required=True,
+                help="Icinga Notification type")
+ap.add_argument("-s", "--state", required=True,
+                help="Service /Host state")
+ap.add_argument("-o", "--output",
+                help="Output for Host notifications, Service Output could be to long!")
+ap.add_argument("-S", "--service", help="If notification is type service")
+ap.add_argument("-e", "--eaglehost", required=True,
+                help="SMSEagle HTTP API Host")
+ap.add_argument("-P", "--protocol", default="http",
+                help="set http protocol: DEFAULT=http")
+ap.add_argument("-d", "--date", required=True,
+                help="display longdatae output from icinga in sms")
+ap.add_argument("-v", "--verbose", action='store_true',
+                help="print verbose messages")
+ap.add_argument("-V", "--version", help="print version", action='store_true')
 args = vars(ap.parse_args())
 
 if args['service']:
@@ -34,8 +46,10 @@ if args['verbose']:
     print message + '\n'
 
 # Build the url query
-base_url = args['protocol'] + '://' + args['eaglehost'] + '/index.php/http_api/send_tocontact'
-query_args = { 'login': args['username'], 'pass': args['password'] , 'contactname': args['contactname'], 'message': message }
+base_url = args['protocol'] + '://' + args['eaglehost']
+base_url += '/index.php/http_api/send_tocontact'
+query_args = {'login': args['username'], 'pass': args['password'],
+              'contactname': args['contactname'], 'message': message}
 encoded_args = urllib.urlencode(query_args)
 
 if args['verbose']:
@@ -51,10 +65,10 @@ result = urllib2.urlopen(url).read()
 if args['verbose']:
     print result + '\n'
 
-### SMSEagle Response
+# SMSEagle Response
 if 'OK' in result:
         print 'Message sent succesfully to ' + args['contactname'] + '\n'
-        os.sys.exit(0)
+        sys.exit(0)
 else:
         print 'Sending SMS failed: ' + result + '\n'
-        os.sys.exit(1)
+        sys.exit(1)
